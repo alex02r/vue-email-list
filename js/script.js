7699af2cf0b851a7daa2855df048a0df10298024
alex02r <132152331+alex02r@users.noreply.github.com>
2023-12-20 16:22:38 +0100

    [UP]Generare e pushare le email nell'array

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 const { createApp } = Vue;
 
 createApp({
-    return: {
-
+    data(){
+        return{
+            emails : [ ]
+        }
+    },
+    created(){
+        for (let i = 1; i <= 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (response) => {
+                this.emails.push(response.data.response);
+            });
+        }
     },
     methods: {
 
